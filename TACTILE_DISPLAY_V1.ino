5f30b18976e1f1e412463fd0986e467ae1212f00
LukeShankland <92568455+LukeShankland@users.noreply.github.com>
2023-09-14 14:06:32 -0700

    Update TACTILE_DISPLAY_V1.ino

--- TACTILE_DISPLAY_V1.ino
+++ TACTILE_DISPLAY_V1.ino
@@ -20,29 +20,23 @@ void setup()
 
   //TD.PLAY_MARIO();
   delay(20);
+  TD.TEST_TCA0();
+  //TD.i2cSCANNER();
   TD.TCA_SCANNER();
 }
 
 void loop() 
 {  
 Serial.println("hi");
-//  delay(1000);    // Delay allows waveform to play, and gives the user time to recognize the display
-//  TD.TEST_TCA1(); // Test TCA1 - Outputs waveform onto 6 ports (0-5)
-//  delay(1000);    // Delay allows waveform to play, and gives the user time to recognize the display
+ // delay(1000);    // Delay allows waveform to play, and gives the user time to recognize the display
+ // TD.TEST_TCA0(); // Test TCA1 - Outputs waveform onto 6 ports (0-5)
+ // delay(1000);    // Delay allows waveform to play, and gives the user time to recognize the display
 
 
 
 //  delay(1000);        // First delay gives user extra time to recognize the display from last repeated (main) loop
-//  TD.PLAY_D_proto(); // Play Braille Char. "D"- Outputs waveform onto designated Tactile Display ports
-//  delay(1000);        // Delay allows waveform to play, and gives the user time to recognize the display
-//  TD.PLAY_A_proto(); // Play Braille Char. "A"- Outputs waveform onto designated Tactile Display ports
-//  delay(1000);        // Delay allows waveform to play, and gives the user time to recognize the display
-//  TD.PLAY_D_proto(); // Play Braille Char. "D"- Outputs waveform onto designated Tactile Display ports
-//  delay(1000);        // Delay allows waveform to play, and gives the user time to recognize the display
+  TD.PLAY_A_proto(); // Play Braille Char. "A"- Outputs waveform onto designated Tactile Display ports
 
-//    delay(500);       // Delay allows waveform to play, and gives the user time to recognize the display
-//    TD.PLAY_SIDE2SIDE();
-//    
     //timestart = micros();
    // TD.LOAD_MAX36();
    // TD.PLAY_MAX36();
@@ -50,11 +44,12 @@ Serial.println("hi");
    // timeend = micros();
    // loadtime = timeend - timestart;
     //delay(100);
-  //TD.PLAY_A_proto();
+  //TD.TEST_TCA0();
   delay(3000);
-  //TD.PLAY_D_proto();
-  TD.READ_WORD();
-  TD.PLAY_WORD("TADC");
+  
+   //TD.PLAY_C_proto();
+  //TD.READ_WORD();
+  //TD.PLAY_WORD();
 /*
     timestart = micros();
     TD.PLAY_MAX36();
